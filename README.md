9c0cb4e4d6a53c2d84a9c769e90d45b66bd57fe7
Justin Dunham <43351446+justin-dunham@users.noreply.github.com>
2019-07-16 12:10:16 -0400

    Create cards.js

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # javascript-examples
-Answers to Interview Questions
+### Answers to Common Questions
+
+These solutions may not be the best way but they are a way. Usually the first solution I thought of that worked.
+
+
+### Contents
+*  [Cards](array/cards.js) - Create a deck of cards and shuffle them.
---
+++ array/cards.js
@@ -0,0 +1,19 @@
+/*
+ *  Deck of Cards Example
+ *  @author: Justin Dunham
+ *  Create a new deck of cards 
+ */
+
+const values = ['2','3','4','5','6','7','8','9','10','J','Q','K','A'];
+const suits = ['S','H','D','C'];
+
+const shuffle = () => Math.random() >= 0.5;
+
+const cards = suits.reduce((accumulator, suit) => {
+  return [
+      ...accumulator,
+        ...values.map(cardValue => `${cardValue}${suit}`)
+  ];
+}, []);
+
+console.log([...cards].sort(shuffle), cards.length);
